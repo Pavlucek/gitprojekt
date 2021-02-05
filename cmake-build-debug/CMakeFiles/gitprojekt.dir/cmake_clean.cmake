d61f98feae06d4b8d3710e06f2c8d82639c281ed
Pavlucek <p.morozewicz99@gmail.com>
2021-02-05 16:09:01 +0100

    git init git add README.md git commit -m "first commit" git branch -M main git remote add origin git@github.com:Pavlucek/gitprojekt.git git push -u origin main

---
+++ cmake-build-debug/CMakeFiles/gitprojekt.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gitprojekt.dir/main.cpp.obj"
+  "gitprojekt.exe"
+  "gitprojekt.exe.manifest"
+  "gitprojekt.pdb"
+  "libgitprojekt.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gitprojekt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
